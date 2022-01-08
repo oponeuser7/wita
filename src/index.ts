6404bd129145e59387609b7cc7ed870600281e22
lavgin7 <oponeuser7@gmail.com>
2022-01-08 23:43:37 +0900

    configure function

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,31 @@ import puppeteer from "puppeteer";
 
 const crawler = async () => {
   try {
-    const result = {};
-    const browser = await puppeteer.launch({ headless: false });
+    const result: Object = {error: null};
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto("http://www.cgv.co.kr/movies/?lt=1&ft=0");
-    page.close();
+    await page.click(".btn-more-fontbold");
+    await page.close();
     browser.close();
     return result;
   }
   catch(e) {
-    return e;
+    console.log(e);
+    return {error: e};
   }
 }
 
-const add = (x: number, y: number) => {
-  return x+y;
-}
-
-const server = http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type':'text/html'});
-	response.end("Hello world!");
-  console.log(add(2,3));
+const server = http.createServer(async function(request, response) {
+  try {
+    const body: Object = await crawler();
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.end(JSON.stringify(body));
+    console.log("you made a response!");
+  }
+  catch(e) {
+    console.log(e);
+  }
 });
 
 server.listen(8888, function() {
